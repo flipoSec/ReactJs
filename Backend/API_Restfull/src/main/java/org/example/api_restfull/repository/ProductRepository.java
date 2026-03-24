a303f56b7c342e732f0d235cb5cc99dc92b14e17
flipoSec <220247641+flipoSec@users.noreply.github.com>
2026-03-24 21:48:50 +0100

    fix product repository

--- Backend/API_Restfull/src/main/java/org/example/api_restfull/repository/ProductRepository.java
+++ Backend/API_Restfull/src/main/java/org/example/api_restfull/repository/ProductRepository.java
@@ -3,10 +3,15 @@ package org.example.api_restfull.repository;
 
 import org.example.api_restfull.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    @Query("SELECT p FROM Product p LEFT JOIN FETCH p.category")
+    List<Product> findAll();
+
+    @Query("SELECT p FROM Product p LEFT JOIN FETCH p.category WHERE p.category.id = :categoryId")
     List<Product> findByCategoryId(Long categoryId);
 }
